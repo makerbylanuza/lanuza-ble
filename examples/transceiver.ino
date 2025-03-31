015e9742b86dc660769985f17e7e9fc068859d5a
makerbylanuza <makerspace@juandelanuza.org>
2025-03-31 16:34:49 +0200

    Update transceiver.ino

--- examples/transceiver.ino
+++ examples/transceiver.ino
@@ -1,55 +1,57 @@
-// Incluimos la biblioteca LanuzaBLE
+// Add LanuzaBLE library
 #include <LanuzaBLE.h>
 
-// Definimos datos para la inicialización
+// Define initialization data
 #define DEVICE_NAME "ESP32_BLE"
 #define SERVICE_UUID "12345678-1234-1234-1234-123456789012"
 #define CHARACTERISTIC_UUID "87654321-4321-4321-4321-098765432109"
 
-// Definimos mensaje y frecuencia de envío
-const String message = "Hola desde ESP32";
-const int freq = 5000;
+// Define message and send frequency
+const String message = "Hello World!";
+const int freq = 5000; // Frequency in miliseconds
 
-// Creamos un objeto de la clase LanuzaBLE
+// Create an object in the LanuzaBLE class
 LanuzaBLE myBLE;
 
-// Definimos qué hacer en los callbacks
+// Define what to do in each callback
 void onConnect() {
-  Serial.println("Dispositivo conectado");
+  Serial.println("A device connected to yours!");
 }
 
 void onDisconnect() {
-  Serial.println("Dispositivo desconectado");
+  Serial.println("A device disconnected from yours...");
 }
 
 void onWrite(String value) {
-  Serial.print("Dato recibido: ");
+  Serial.print("Received data: ");
   Serial.println(value);
 }
 
 void setup() {
+  // Initialize Serial Monitor
   Serial.begin(115200);
+  delay(1000);
     
-  // Iniciar BLE con nombre del dispositivo, UUID del servicio y característica
+  // Initialize BLE device using the device name and the service and characteristic UUIDs
   myBLE.begin(DEVICE_NAME, SERVICE_UUID, CHARACTERISTIC_UUID);
 
-  // Mostrar la dirección MAC del dispositivo
+  // Show ESP32 MAC address
   String macAddress = myBLE.getMACAddress();
-  Serial.print("Dirección MAC del ESP32: ");
+  Serial.print("ESP32 MAC address: ");
   Serial.println(macAddress);
 
-  // Definir callbacks
+  // Register callbacks
   myBLE.onConnect(onConnect);
   myBLE.onDisconnect(onDisconnect);
   myBLE.onWrite(onWrite);
 
-  // Iniciar publicidad BLE
+  // Initialize BLE advertising (start communicating)
   myBLE.startAdvertising();
-  Serial.println("Esperando conexiones BLE...");
+  Serial.println("Waiting for BLE connections...");
 }
 
 void loop() {
-  // Enviamos el mensaje y esperamos
+  // Send message and wait
   myBLE.sendData(message);
   delay(freq);
 }
